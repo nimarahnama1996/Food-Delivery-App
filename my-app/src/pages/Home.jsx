c308f45f4c07f47443e18cd84fbc949e975a6a24
nimarahnama1996 <n.rahnama1996@gmail.com>
2022-11-08 17:33:03 +0330

    adding header and footer

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,4 +1,37 @@
 import React from 'react'
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+
+
+
+import Helmet from "../components/Helmet/Helmet.js";
+import featureImg01 from "../assets/images/service-01.png";
+import featureImg02 from "../assets/images/service-02.png";
+import featureImg03 from "../assets/images/service-03.png";
+
+
+
+
+
+const featureData = [
+  {
+    title: "Quick Delivery",
+    imgUrl: featureImg01,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+
+  {
+    title: "Super Dine In",
+    imgUrl: featureImg02,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+  {
+    title: "Easy Pick Up",
+    imgUrl: featureImg03,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+];
+
+
 
 const Home = () => {
   return (
